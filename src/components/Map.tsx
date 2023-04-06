758cd58e7cfb9b51f5bdc3e3cc137616efb6cf7b
tlc2880 <tommylcao@gmail.com>
2023-04-06 13:18:20 -0700

    Fetch NASA EONET data

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,31 +1,40 @@
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+type IeventData = {
+    ev: {},
+    index: number
+}
 
 type MapProps = {
-    eventData: {
-        ev: {},
-        index: number
-    },
+    eventData: IeventData[],
     center: {
         lat: number,
         lng: number
     },
     zoom: number
-} & typeof defaultProps;
+};
 
-const defaultProps = {
-    eventData: {
-        ev: {},
-        index: 0
-    },
-    center: {
-        lat: 42.3265,
-        lng: -122.8756
-    },
-    zoom: 6
-}
+// const defaultProps = {
+//     eventData: {
+//         ev: {},
+//         index: 0
+//     },
+//     center: {
+//         lat: 42.3265,
+//         lng: -122.8756
+//     },
+//     zoom: 6
+// }
 
 const Map = (props: MapProps) => {
+    const markers = props.eventData.map((ev: any) => {
+        if (ev.categories[0].id === 8) {
+            return <LocationMarker lat={ev.geometries[0].coordinates[1]} 
+            lng={ev.geometries[0].coordinates[0]} />
+        }
+        return null
+    })
+
     return (
         <div className="map">
             <GoogleMapReact
@@ -34,10 +43,11 @@ const Map = (props: MapProps) => {
                 defaultZoom={ props.zoom }
             >
                 <LocationMarker lat={props.center.lat} lng={props.center.lng} />
+                {markers}
              </GoogleMapReact>
          </div>
     )
 }
 
-Map.defaultProps = defaultProps;
+//Map.defaultProps = defaultProps;
 export default Map
